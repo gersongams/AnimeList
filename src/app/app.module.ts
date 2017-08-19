718be41bed17d0a1e859ac94aea78ab1d9955aff
Gerson <ggarridom@uni.pe>
2017-08-18 23:29:50 -0500

    adding boostrap4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AnimesComponent } from './animes/animes.component';
+import { AnimeSearchComponent } from './anime-search/anime-search.component';
+import { AnimeDetailComponent } from './anime-detail/anime-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, DashboardComponent, AnimesComponent, AnimeSearchComponent, AnimeDetailComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot() // Add Bootstrap module here.
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
